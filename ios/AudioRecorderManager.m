8ccc70379557748d3440e1b5d602e6837f1575b4
hanbitkim <47418027+hanbitkim@users.noreply.github.com>
2019-05-02 11:26:07 +0900

    Update AudioRecorderManager.m

--- ios/AudioRecorderManager.m
+++ ios/AudioRecorderManager.m
@@ -230,7 +230,7 @@ RCT_EXPORT_METHOD(prepareRecordingAtPath:(NSString *)path sampleRate:(float)samp
       [_recordSession setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionAllowBluetooth error:nil];
       [_recordSession setMode:AVAudioSessionModeMeasurement error:nil];
   }else{
-      [_recordSession setCategory:AVAudioSessionCategoryMultiRoute error:nil];
+      [_recordSession setCategory:AVAudioSessionCategoryRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker | AVAudioSessionCategoryOptionAllowBluetooth error:nil];
   }
 
   _audioRecorder = [[AVAudioRecorder alloc]
